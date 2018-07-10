c22d673c3e586a0bb66afa36b28936782bb6a299
Andrew <andrew.j.lines@gmail.com>
2018-07-10 02:31:53 +0000

    Added description to cmpgnd model. Added detailed cmpgnd info page. Refactored for more RESTful routing

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ mongoose.connect("mongodb://localhost/yelpcamp");
 
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -28,13 +29,7 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 //                   VARIABLES
 // ----------------------------------------------
 
-// var campgrnds = [
-//         {name: "Deep Creek", image: "https://pixabay.com/get/ef3cb00b2af01c22d2524518b7444795ea76e5d004b0144295f8c67ba0e9b1_340.jpg"},
-//         {name: "Borough Huts", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg"},
-//         {name: "Mt Stapylton", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f0c97ea5eab1bc_340.jpg"},
-//         {name: "Joker's Flat", image: "https://farm9.staticflickr.com/8457/7930235502_df747573ca.jpg"},
-//         {name: "Tidal River", image: "https://farm1.staticflickr.com/82/225912054_690e32830d.jpg"}
-//         ];
+
         
 // ----------------------------------------------
 //                   ROUTES
@@ -44,6 +39,7 @@ app.get("/", function(req,res){
     res.render("home");
 });
 
+// INDEX - Show the listing of all the campgrounds
 app.get("/campgrounds", function(req,res){
     // Retrieve all of the campgrounds from the database
     Campground.find({}, function(err, campgrnds){
@@ -51,23 +47,22 @@ app.get("/campgrounds", function(req,res){
             console.log(err);
         } else {
             // display all of the campgrounds on the main page
-            res.render("campgrounds", {sites: campgrnds});
+            res.render("index", {sites: campgrnds});
         }
     });
 });
 
+// UPDATE - Create an entry in the database for a new campground
 app.post("/campgrounds", function(req,res){
-    // get data from form and add to campgrounds around
-    var name = req.body.name;
-    var img = req.body.img;
+    // get data from form and build new object
+    var newCampgnd = {
+        name: req.body.name,
+        image: req.body.img,
+        description: req.body.desc
+    };
     
     // Add a new campground to the database
-    Campground.create(
-        {
-            name: name, 
-            image: img    
-        }, 
-        function(err,newCampgnd){
+    Campground.create(newCampgnd, function(err,newCampgnd){
             if(err){
                 console.log(err);
             } else {
@@ -78,9 +73,18 @@ app.post("/campgrounds", function(req,res){
     
 });
 
+// NEW - show the form for adding a new campground to the database
 app.get("/campgrounds/new", function(req,res){
-    res.render("newcmpgrd")
-})
+    res.render("new");
+});
+
+//SHOW - show the page for detailed information about a specific campsite
+app.get("/campgrounds/:id", function(req,res){
+    // retrieve the campground object from the database
+    var campground = Campground.findByID(req.params.id);
+    // show the campground details page
+    res.render("show", {campground: campground});
+});
 
 // ----------------------------------------------
 //                   SERVER
